f3b6ab60081127455de9e14f9549c9b474889627
Adam Brutsaert <adam.brutsaert@epitech.eu>
2023-04-01 17:00:19 +0200

    [~] Extracted tests logic in lib.rs

--- tests/basic_boot.rs
+++ tests/basic_boot.rs
@@ -5,27 +5,14 @@
 #![reexport_test_harness_main = "test_main"]
 
 use core::panic::PanicInfo;
-use my_os::println;
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    println!("Hello World{}", "!");
-
-    #[cfg(test)]
     test_main();
-
     loop {}
 }
 
-#[cfg(not(test))]
 #[panic_handler]
-fn panic(info: &PanicInfo) -> ! {
-    println!("{}", info);
-    loop {}
-}
-
-#[cfg(test)]
-#[panic_handler]
-fn panic(info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> !  {
     my_os::test_panic_handler(info)
 }
